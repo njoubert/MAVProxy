3cb6cfcc22b6d809a554f54e77a3c79516c476f4
Niels Joubert <njoubert@gmail.com>
2017-07-21 16:25:31 -0700

    speech: hackey priotity implementation

--- MAVProxy/modules/mavproxy_speech.py
+++ MAVProxy/modules/mavproxy_speech.py
@@ -24,7 +24,7 @@ class PeriodicSpeechAnnounce(object):
     capabilities.
     '''
 
-    def __init__(self, module, msg_type, entry_name, sayEveryS=30.0, sayAs=None, scale=1, formatstr="%0.0f"):
+    def __init__(self, module, msg_type, entry_name, sayEveryS=30.0, sayAs=None, scale=1, formatstr="%0.0f", voice=None):
         self.module = module
         self.msg_type = msg_type
         self.entry_name = entry_name
@@ -32,6 +32,8 @@ class PeriodicSpeechAnnounce(object):
         self.sayAs = sayAs
         self.scale = scale
         self.formatstr = formatstr
+        self.voice = voice
+        self.priority = 'normal'
 
         self.lastTime = 0
 
@@ -47,6 +49,12 @@ class PeriodicSpeechAnnounce(object):
     def set_format(self, formatstr):
         self.formatstr = formatstr
 
+    def set_voice(self, voice):
+        self.voice = voice
+
+    def set_priority(self, priority):
+        self.priority = priority
+
     def match(self, msg):
         '''Returns true if this announce is ready to handle the given message'''
         return (msg.get_type() == self.msg_type)
@@ -59,13 +67,15 @@ class PeriodicSpeechAnnounce(object):
     def msg_entry_to_text(self, msg):
         return getattr(msg, self.entry_name, 0)
 
-    def maybe_say(self, msg, backend):
+    def maybe_say(self, msg, backend, force=False):
         '''Calls backend with this announce for the given message IF the announce passes internal checks'''
         now = time.time()
-        if (now - self.lastTime > self.sayEveryS):
+        if force or (now - self.lastTime > self.sayEveryS):
             self.lastTime = now
             value = self.msg_entry_to_text(msg)
-            backend(self.to_speech_text(value))
+            backend(self.to_speech_text(value), priority=self.priority, voice=self.voice)
+            return True
+        return False
 
     def to_speech_text(self, value):
         '''Returns the text announced before the value'''
@@ -75,20 +85,24 @@ class PeriodicSpeechAnnounce(object):
         if self.sayAs:
             return self.sayAs
         else:
-            return self.entry_name
+            return ""
 
     def convert(self, value):
         '''Returns the announce text representation of the value'''
         return self.formatstr % value #(value*self.scale)
 
     def __str__(self):
-        return "%s.%s as \"%s\" every %0.0f seconds, scaling by %0.4f and formatting as %s" % (
+        voice_str = "default"
+        if (self.voice):
+            voice_str = self.voice
+        return "%s.%s as \"%s\" every %0.0f seconds, scaling by %0.4f and formatting as %s with %s voice" % (
             self.msg_type, 
             self.entry_name, 
             self.get_speech_name(), 
             self.sayEveryS,
             self.scale,
-            self.formatstr)
+            self.formatstr,
+            voice_str)
 
 class ConditionalSpeechAnnounce(PeriodicSpeechAnnounce):
     '''
@@ -96,9 +110,10 @@ class ConditionalSpeechAnnounce(PeriodicSpeechAnnounce):
     only when a mavlink value is outside an expected range.
     '''
 
-    def __init__(self, module, msg_type, entry_name, sayEveryS=5.0, sayAs=None, scale=1, formatstr="%0.0f", range=[-float("inf"), float("inf")]):
-        super(ConditionalSpeechAnnounce, self).__init__(module, msg_type, entry_name, sayEveryS=sayEveryS, sayAs=sayAs, scale=scale, formatstr=formatstr)
+    def __init__(self, module, msg_type, entry_name, sayEveryS=5.0, sayAs=None, scale=1, formatstr="%0.0f", voice=None, range=[-float("inf"), float("inf")]):
+        super(ConditionalSpeechAnnounce, self).__init__(module, msg_type, entry_name, sayEveryS=sayEveryS, sayAs=sayAs, scale=scale, formatstr=formatstr, voice=None)
         self.range = range
+        self.set_voice("Daniel")
 
     def set_min(self, minval):
         self.range[1] = minval
@@ -106,10 +121,11 @@ class ConditionalSpeechAnnounce(PeriodicSpeechAnnounce):
     def set_max(self, maxval):
         self.range[1] = maxval
 
-    def maybe_say(self, msg, backend):
+    def maybe_say(self, msg, backend, force=False):
         value = self.msg_entry_to_text(msg)
-        if value < self.range[0] or value > self.range[1]:
-            super(ConditionalSpeechAnnounce, self).maybe_say(msg, backend)
+        if force or value < self.range[0] or value > self.range[1]:
+            return super(ConditionalSpeechAnnounce, self).maybe_say(msg, backend, force=force)
+        return False
 
     def to_speech_text(self, value):
         if value < self.range[0]:
@@ -129,25 +145,68 @@ class OnChangeSpeechAnnounce(PeriodicSpeechAnnounce):
     only when a mavlink value changes.
     '''
 
-    def __init__(self, module, msg_type, entry_name, sayEveryS=5.0, sayAs=None, scale=1, formatstr="%0.0f", threshold=0):
-        super(OnChangeSpeechAnnounce, self).__init__(module, msg_type, entry_name, sayEveryS=sayEveryS, sayAs=sayAs, scale=scale, formatstr=formatstr)
+    def __init__(self, module, msg_type, entry_name, sayEveryS=5.0, sayAs=None, scale=1, formatstr="%0.0f", voice=None, threshold=0):
+        super(OnChangeSpeechAnnounce, self).__init__(module, msg_type, entry_name, sayEveryS=sayEveryS, sayAs=sayAs, scale=scale, formatstr=formatstr, voice=None)
         self.prev_value = float("inf")
         self.threshold = threshold
-
+        self.set_voice("Daniel")
+        self.set_priority('important')
+        
     def set_threshold(self, threshold):
         self.threshold = threshold
 
-    def maybe_say(self, msg, backend):
+    def maybe_say(self, msg, backend, force=False):
         new_value = self.msg_entry_to_text(msg)
         if self.prev_value == float("inf"):
             self.prev_value = new_value
-        if (self.threshold == 0 and self.prev_value != new_value) or (self.threshold > 0 and abs(self.prev_value - new_value) >= self.threshold):
+        if force or (self.threshold == 0 and self.prev_value != new_value) or (self.threshold > 0 and abs(self.prev_value - new_value) >= self.threshold):
             self.prev_value = new_value
-            super(OnChangeSpeechAnnounce, self).maybe_say(msg, backend)
+            super(OnChangeSpeechAnnounce, self).maybe_say(msg, backend, force=force)
 
     def __str__(self):
         s = super(OnChangeSpeechAnnounce, self).__str__()
         return "%s when value changes by more than %f" % (s, self.threshold)  
+
+class PeriodicOnChangeSpeechAnnounce(PeriodicSpeechAnnounce):
+    '''
+    A SpeechAnnounce subclass that provides rate-limited announcements
+    only when a mavlink value changes.
+    '''
+
+    def __init__(self, module, msg_type, entry_name, sayEveryS=5.0, sayAs=None, scale=1, formatstr="%0.0f", voice=None, threshold=0):
+        super(PeriodicOnChangeSpeechAnnounce, self).__init__(module, msg_type, entry_name, sayEveryS=sayEveryS, sayAs=sayAs, scale=scale, formatstr=formatstr, voice=None)
+        self.prev_value = float("inf")
+        self.threshold = threshold
+        self.set_voice(None)
+        self.ratelimit = 0
+
+    def set_threshold(self, threshold):
+        self.threshold = threshold
+
+    def set_ratelimit(self, ratelimit):
+        self.ratelimit = ratelimit
+
+    def maybe_say(self, msg, backend, force=False):
+        new_value = self.msg_entry_to_text(msg)
+        if self.prev_value == float("inf"):
+            self.prev_value = new_value
+
+        now = time.time()
+        if not force and (now - self.lastTime < self.ratelimit):
+            return
+
+        if not(super(PeriodicOnChangeSpeechAnnounce, self).maybe_say(msg, backend, force)):
+            if (self.threshold == 0 and self.prev_value != new_value) or (self.threshold > 0 and abs(self.prev_value - new_value) >= self.threshold):
+                self.prev_value = new_value
+                super(PeriodicOnChangeSpeechAnnounce, self).maybe_say(msg, backend, force=True)
+
+        else:
+            self.prev_value = new_value
+
+    def __str__(self):
+        s = super(PeriodicOnChangeSpeechAnnounce, self).__str__()
+        return "%s periodically AND when value changes by more than %f (rate-limited to %fs)" % (s, self.threshold, self.ratelimit)  
+
 
 import threading
 import Queue
@@ -164,16 +223,26 @@ class SaySpeechBackend(threading.Thread):
         self.to_say_queue = Queue.Queue()
         self.daemon = True
 
-    def say(self, text, priority='important'):
-        self.to_say_queue.put(text)
+    def say(self, text, priority='normal', voice=None):
+        if (self.to_say_queue.qsize() > 2):
+            print "FLUSHING TO_SAY_QUEUE. Size is ", self.to_say_queue.qsize()
+            with self.to_say_queue.mutex:
+                self.to_say_queue.queue.clear()
+        if (priority == 'important'):
+            with self.to_say_queue.mutex:
+                self.to_say_queue.queue.clear()
+        self.to_say_queue.put((text, priority, voice))
 
     def run(self):
         '''thread loop here'''
         while True:
             try:
                 # We loop once a second and give MAVProxy a chance to kill threads.
-                text = self.to_say_queue.get(True, 1.0)
-                subprocess.call(['say', text])
+                (text, priority, voice) = self.to_say_queue.get(True, 1.0)
+                if (voice):
+                    subprocess.call(['say', '-v', voice, text])
+                else:
+                    subprocess.call(['say', text])
                 self.to_say_queue.task_done()
             except Queue.Empty:
                 pass
@@ -239,7 +308,7 @@ class SpeechModule(mp_module.MPModule):
     # Different speech backends.
     #
 
-    def say_using_speechd(self, text, priority='important'):
+    def say_using_speechd(self, text, priority='important',voice=None):
         '''speak some text'''
         ''' http://cvs.freebsoft.org/doc/speechd/ssip.html see 4.3.1 for priorities'''
         import speechd
@@ -251,26 +320,26 @@ class SpeechModule(mp_module.MPModule):
         self.speech.speak(text)
         self.speech.close()
 
-    def say_using_espeak(self, text, priority='important'):
+    def say_using_espeak(self, text, priority='important',voice=None):
         '''speak some text using espeak'''
         from espeak import espeak
         if self.settings.speech_voice:
             espeak.set_voice(self.settings.speech_voice)
         espeak.synth(text)
 
-    def say_using_speech(self, text, priority='important'):
+    def say_using_speech(self, text, priority='important',voice=None):
         '''speak some text using speech module'''
         import speech
         speech.say(text)
 
-    def say_using_say(self, text, priority='important'):
+    def say_using_say(self, text, priority='important', voice=None):
         '''speak some text using macOS say command'''
         if self.say_speech_backend is None:
             subprocess.call(['say', ""]) #Will throw an exception if say doesn't exist
             self.say_speech_backend = SaySpeechBackend()
             self.say_speech_backend.start()
 
-        self.say_speech_backend.say(text, priority)
+        self.say_speech_backend.say(text, priority, voice)
         
     #
     # External Interface
@@ -289,18 +358,18 @@ class SpeechModule(mp_module.MPModule):
             for announcements in self.msgs_to_announce:
                 announcements.dispatch(msg, self.say)
 
-    def print_and_say(self, text, priority='important'):
+    def print_and_say(self, text, priority='important', voice=None):
         self.console.writeln(text)
         if self.enabled_for == "ALL":
-            self.say(text, priority)
+            self.say(text, priority, voice=voice)
         elif self.enabled_for == "MODE" and len(text) > 4 and text[0:4] == "Mode":
-            self.say(text, priority)
+            self.say(text, priority='important', voice="Daniel")
 
-    def say(self, text, priority='important'):
+    def say(self, text, priority='important', voice=None):
         '''speak some text'''
         ''' http://cvs.freebsoft.org/doc/speechd/ssip.html see 4.3.1 for priorities'''
         if self.settings.speech and self.say_backend is not None:
-            self.say_backend(text, priority=priority)
+            self.say_backend(text, priority=priority, voice=voice)
 
     #
     # Command line configuration commands.
@@ -308,13 +377,15 @@ class SpeechModule(mp_module.MPModule):
     #
     def cmd_speech(self, args):
         '''speech commands'''
-        usage =  "usage: speech <say|list|enable|add|remove>\n"
-        usage += "  Call any command with no arguments for details"
-        if len(args) < 1:
-            print(usage)
-            return
-
-        if args[0] == "say":
+        if len(args) < 1 or args[0] == "list":
+            print "usage: speech <say|list|enable|add|remove>"
+            print "  Call any command with no arguments for details"
+            print ""
+            print "Speech module will announce:", self.enabled_for
+            print "Speech module knows how to report: "
+            for idx, announcement in enumerate(self.msgs_to_announce):
+                print "#%d: %s" % (idx, announcement)
+        elif args[0] == "say":
             cmd_usage  = "usage: speech say <text to say>\n"
             cmd_usage += "  Say arbitrary text. Useful for testing"
             if len(args) < 2:
@@ -339,16 +410,13 @@ class SpeechModule(mp_module.MPModule):
                         self.settings.set('speech', 1)
                 else:
                     print cmd_usage
-        elif args[0] == "list":
-            print "Speech module will announce:", self.enabled_for
-            print "Speech module knows how to report: "
-            for idx, announcement in enumerate(self.msgs_to_announce):
-                print "#%d: %s" % (idx, announcement)
         elif args[0] == "add":
             usage =  "usage: speech\n"
-            usage += "add <msg_type>.<msg_entry> periodic    (every=<seconds>) (as=<say_as>) (scale=<scale>) (format=\"<formatstr>\")\n"
-            usage += "add <msg_type>.<msg_entry> conditional (every=<seconds>) (as=<say_as>) (scale=<scale>) (format=\"<formatstr>\") (max=<val>) (min=<val>)\n"
-            usage += "add <msg_type>.<msg_entry> onchange    (every=<seconds>) (as=<say_as>) (scale=<scale>) (format=\"<formatstr>\") (threshold=<threshold>)"
+            usage += "add <msg_type>.<msg_entry> periodic          (every=<seconds>) (as=<say_as>) (scale=<scale>) (format=<formatstr>) (voice=<voice>) \n"
+            usage += "add <msg_type>.<msg_entry> conditional       (every=<seconds>) (as=<say_as>) (scale=<scale>) (format=<formatstr>) (voice=<voice>) (max=<val>) (min=<val>)\n"
+            usage += "add <msg_type>.<msg_entry> onchange          (every=<seconds>) (as=<say_as>) (scale=<scale>) (format=<formatstr>) (voice=<voice>) (threshold=<threshold>)"
+            usage += "add <msg_type>.<msg_entry> periodic_onchange (every=<seconds>) (as=<say_as>) (scale=<scale>) (format=<formatstr>) (voice=<voice>) (threshold=<threshold>) (ratelimit=<limit in seconds>)"
+            
             if len(args) < 3:
                 print usage
             else:
@@ -363,6 +431,8 @@ class SpeechModule(mp_module.MPModule):
                         announce = ConditionalSpeechAnnounce(self, msg_type, entry_name)
                     elif what_type == "onchange":
                         announce = OnChangeSpeechAnnounce(self, msg_type, entry_name)
+                    elif what_type == "periodic_onchange":
+                        announce = PeriodicOnChangeSpeechAnnounce(self, msg_type, entry_name)
                     else:
                         print "Need a type: periodic, conditional or onchange"
                         print usage
@@ -386,6 +456,10 @@ class SpeechModule(mp_module.MPModule):
                                 announce.set_min(float(value))
                             if name == "threshold":
                                 announce.set_threshold(float(value))
+                            if name == "voice":
+                                announce.set_voice(str(value))
+                            if name == "ratelimit":
+                                announce.set_ratelimit(float(value))
                             optargs = optargs[1::]
 
                     self.msgs_to_announce.append(announce)
